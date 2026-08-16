2a67a2fb418bca2e2dc234303707316e8df05354
Chadeck <chadowd5@gmail.com>
2026-08-16 22:46:06 +0200

    ajouter un nouveau fichier

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,27 @@
 const loadText = document.querySelector('.text')
 const btn = document.getElementById('#view')
+const textHeader = document.querySelector('.textHeader')
 
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
             console.log("visible");
             loadText.classList.add('view')
-            btn.classList.add('hide')
+
         }
     })
 })
 observer.observe(loadText)
+// header observer
+
+window.addEventListener('DOMContentLoaded',()=>{
+    const view = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if(entry.isIntersecting){
+                alert('element visible')
+                textHeader.classList.add('textHeader')
+            }
+        })
+    })
+})
+view.observe(textHeader)
