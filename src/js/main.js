28f3cb953edf573cde02ddcf10ee910f36e1dfd5
Chadeck <chadowd5@gmail.com>
2026-08-22 16:27:10 +0200

    nouvelle version du project pour la performance

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
 const loadText = document.querySelector('.text')
-const btn = document.getElementById('#view')
+const btn = document.getElementById('view')
 const textHeader = document.querySelector('.textHeader')
-
+const textRaison = document.querySelectorAll('.ask p')
+const elements= document.querySelectorAll('.para')
+const carte = document.querySelector('.carte')
+const slider = document.getElementById('home')
+const pass = document.querySelector('.pass')
+const retour = document.querySelector('.return')
+// accueil 
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
@@ -11,17 +17,44 @@ const observer = new IntersectionObserver((entries)=>{
         }
     })
 })
-observer.observe(loadText)
+ if (loadText) {
+  observer.observe(loadText); 
+}
+
 // header observer
 
 window.addEventListener('DOMContentLoaded',()=>{
     const view = new IntersectionObserver((entries)=>{
         entries.forEach((entry)=>{
             if(entry.isIntersecting){
-                alert('element visible')
                 textHeader.classList.add('textHeader')
             }
         })
     })
+  if (textHeader) {
+  view.observe(textHeader); 
+}
 })
-view.observe(textHeader)
+
+// raison
+const paraObseve = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+           entry.target.classList.add('paraView') 
+        }
+    })
+})
+textRaison.forEach((el)=> paraObseve.observe(el))
+// slider
+let position = 370
+pass.addEventListener('click',()=>{
+    position+=370
+    slider.style.transform = `translateX(-${position}px)`
+})
+//return
+retour.addEventListener('click',()=>{
+   position -= 370
+   slider.style.transform =`translateX(-${position}px)`  
+})
+
+
